72a7e8517712f237af4d4aeb03de759e316cf3d2
Gajus Kuizinas <gk@anuary.com>
2016-01-04 17:58:03 +0000

    Attempt to prevent prepublish script running after npm install.

--- src/commands/prepublish.js
+++ src/commands/prepublish.js
@@ -11,6 +11,18 @@ import {
 } from './../utils';
 
 export default () => {
+    // NPM will run prepublish script after `npm install` (https://docs.npmjs.com/misc/scripts)
+    // This ensures that when script is executed using `npm *` it is run only when the command is `npm publish`.
+    if (process.env.npm_config_argv) {
+        let npmConfigArgv;
+
+        npmConfigArgv = JSON.parse(process.env.npm_config_argv);
+
+        if (npmConfigArgv.original[0] !== 'publish') {
+            return;
+        }
+    }
+
     backupPackageConfig();
 
     fs.renameSync(nodeModulesPath, nodeModulesBackupPath);
